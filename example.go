92374b4c636abd89892512838353b21be433cfac
song <1334359917@qq.com>
2020-07-13 12:18:20 +0800

    comment

--- example.go
+++ example.go
@@ -5,9 +5,8 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/ClearGrass/OpenapiSdkGo/structs"
-
 	"github.com/ClearGrass/OpenapiSdkGo/openapi"
+	"github.com/ClearGrass/OpenapiSdkGo/structs"
 )
 
 func main() {
@@ -15,7 +14,7 @@ func main() {
 	authPath := "http://oauth.test.cleargrass.com/oauth2/token"
 	accessKey := "8XL4IBGGR"
 	secretKey := "b34bc5bfbf5611eabd0200163e2c48b3"
-	client := openapi.NewClient(apiHost, authPath, accessKey, secretKey)
+	client := openapi.NewClient(apiHost, authPath, accessKey, secretKey) // 建议调用方将client设置为单例
 
 	// 设备列表
 	res, err := client.DeviceList(context.Background())
@@ -33,7 +32,7 @@ func main() {
 		mac := res.Devices[0].Info.Mac
 		startTime := time.Now().AddDate(0, 0, -1).Unix()
 		filter := &structs.QueryDeviceDataReq{Mac: mac, StartTime: startTime}
-		data, err := client.DeviceData(context.Background(), filter)
+		data, err := client.QueryDeviceData(context.Background(), filter)
 		if err != nil {
 			panic(err)
 		}
